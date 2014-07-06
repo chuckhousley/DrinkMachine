288d44c2841285c9f703fd45fd04cc11870ca8a9
Chuck <charles@charleshousley.com>
2014-07-06 07:59:19 -0500

    trying out new tutorial

--- drink.py
+++ drink.py
@@ -7,27 +7,33 @@ as published by Sam Hocevar. See the COPYING file for more details.
 """
 
 import os
-import drinkDB
-from views import DrinkView
-from flask import Flask, request, session, g
+from flask import Flask, request, session, g, render_template
+from flask.ext.restless import APIManager
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # create application
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drink.db'
+db = SQLAlchemy(app)
 
-# load up a config
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'drink.db'),
-    DEBUG=True,
-    SECRET_KEY='change this sometime',
-    USERNAME='admin',
-    PASSWORD='admin'
-    ))
-app.config.from_envvar('DRINK_SETTINGS', silent=True)
 
-app.add_url_rule('/', view_func=DrinkView.as_view('drink_view'))
+class Drink(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.TEXT)
+    num = db.Column(db.INTEGER)
+
+db.create_all()
+
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(Drink, methods=['GET', 'POST', 'DELETE', 'PUT'])
+
+
+@app.route('/')
+def hello():
+    return render_template('main.html')
+
 
 if __name__ == "__main__":
-    app.run(host='10.10.56.190')
+    app.run(debug=True)  # host='10.10.56.190')
 
 
