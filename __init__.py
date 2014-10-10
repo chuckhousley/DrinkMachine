b9c863d35fc495093804fa4259775da10687b4f5
Chuck <charles@charleshousley.com>
2014-10-10 15:15:38 -0500

    remove from remote, whoops

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-from flask import Flask, request, render_template
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drink.db'
-app.debug = True
-from controllers import *
--- controller/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.restless import APIManager
-from drink-machine import app
-import model.models
-
-db = SQLAlchemy(app)
-api_manager = APIManager(app, flask_sqlalchemy_db=db)
-api_manager.create_api(models.Drink, methods=['GET'])
