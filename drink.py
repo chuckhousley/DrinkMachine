ba1494afb962fb8fba84e306cfb4c26a83602b6d
Chuck Housley <chousley1@gmail.com>
2014-10-14 11:41:12 -0500

    update license

--- drink.py
+++ drink.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Copyright (C) 2014 Chuck Housley
-This work is free. You can redistribute it and/or modify it under the
-terms of the Do What The Fuck You Want To Public License, Version 2,
-as published by Sam Hocevar. See the COPYING file for more details.
-"""
-
 import os
 from server import app, db
 import server.model
