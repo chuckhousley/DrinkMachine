f022595dd76fb2cfb2dad1a30eb34a50dde6a934
Chuck <charles@charleshousley.com>
2014-11-03 22:07:08 -0600

    thinking for myself, this should be better

--- server/model/models.py
+++ server/model/models.py
@@ -1,10 +1,11 @@
 from server import db
 
-instructions = db.Table('Instructions',
-	db.Column('drink_id', db.INTEGER, db.ForeignKey('drink.id')),
-	db.Column('bottle_id', db.INTEGER, db.ForeignKey('bottle.id')),
-	db.Column('amount', db.INTEGER, nullable=False)
-)
+class Instructions(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True)
+    drink_id = db.Column(db.INTEGER, db.ForeignKey('drink.id'), nullable=False)
+    bottle_id = db.Column(db.INTEGER, db.ForeignKey('bottle.id'), nullable=False)
+    amount = db.Column(db.INTEGER, nullable=False)
+
 
 class Drink(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
@@ -22,19 +23,19 @@ class Drink(db.Model):
 
 
 class Bottle(db.Model):
-	id = db.Column(db.INTEGER, primary_key=True)
-	name = db.Column(db.TEXT)
-	amount = db.Column(db.INTEGER)
-	
-	
-	def __init__(self, amount):
-		self.name = name
-		self.amount = amount
-		
-	def __repr__(self):
-		return self
-		
-		
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.TEXT)
+    amount = db.Column(db.INTEGER)
+    
+    
+    def __init__(self, amount):
+        self.name = name
+        self.amount = amount
+        
+    def __repr__(self):
+        return self
+        
+        
 class Machine(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
     bottle = db.Column(db.INTEGER, db.ForeignKey('bottle.id'), unique=True)
